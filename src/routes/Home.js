c2b49cebc1acd99b83df661d6460f1d822b51cb2
DaeLee <masterkorea01@naver.com>
2020-06-08 17:22:38 +0900

    react-redux

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,46 @@
-export default () => "Home";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addToDo } from "../store";
+import ToDo from "../components/ToDo";
+
+function Home({ toDos, dispatchAddToDo }) {
+  console.log({ toDos });
+  const [text, setText] = useState("");
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    dispatchAddToDo(text);
+    setText("");
+  }
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange}></input>
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatchAddToDo: (text) => dispatch(addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
