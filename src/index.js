9bd8645c2a0101d8bd06b8de9b5d37013f382c3e
DaeLee <masterkorea01@naver.com>
2020-06-08 16:20:26 +0900

    finishing with Vanilla JS Redux and will start with React-Redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
-import App from "./App";
-import { createStore } from "redux";
-import { act } from "react-dom/test-utils";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
-  }
-};
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
-
-const countStore = createStore(countModifier);
-countStore.subscribe(onChange);
-
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+ReactDOM.render(<App></App>, document.getElementById("root"));
