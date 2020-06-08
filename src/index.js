d97fb095535b78e3c1b7649fe5293d183d8722ae
DaeLee <masterkorea01@naver.com>
2020-06-08 13:10:02 +0900

    Redux Actions only with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 import App from "./App";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
